27f81aa302bdb2c4d9bbbc534b6ee549eb9c47b0
David Ernst <git@dsernst.com>
2015-06-30 17:18:38 -0700

    working zip downloader

--- archiver.js
+++ archiver.js
@@ -1,6 +1,9 @@
 require('dotenv').load();
 var AWS = require('aws-sdk');
 var Promise = require('bluebird');
+var mkdirp = require('mkdirp');
+var fs = Promise.promisifyAll(require('fs'));
+var archiver = require('archiver');
 
 // AWS SDK Initialization
 var s3 = new Promise.promisifyAll(new AWS.S3({
@@ -8,26 +11,54 @@ var s3 = new Promise.promisifyAll(new AWS.S3({
   secretAccessKey: process.env.AWS_Secret_Access_Key
 }));
 
-var bucketName = 'coding-challenges';
+// Make sure we have a local debug folder
+// mkdirp.sync('./debug/' + filePrefix + projectName + fileSuffix);
 
-var filePrefix = 'file_archiving/';
-var projectName = 'small';
-var fileSuffix = '_project/';
+function getProjectFilesAsZip(projectName) {
+  // 1. Given a project name "small", "medium", "large", find the files that match the prefix
+  var bucketName = 'coding-challenges';
 
-// 1. Given a project name "small", "medium", "large", find the files that match the prefix
-s3.listObjectsAsync({Bucket: bucketName})
-  .get('Contents')
-  // .tap(console.log)
-  .filter(function (file) {
-    return file.Key.indexOf(filePrefix + projectName + fileSuffix) === 0;
-  })
-  // .tap(console.log)
-  // 2. Download raw files from S3
-  .map(function (file) {
-    return s3.getSignedUrlAsync('getObject', {Bucket: bucketName, Key: file.Key})
-  })
-  .tap(console.log)
+  var filePrefix = 'file_archiving/';
+  projectName = projectName || 'small';
+  var fileSuffix = '_project/';
+  return s3.listObjectsAsync({Bucket: bucketName})
+    .get('Contents')
+    // .tap(console.log)
+    .filter(function (file) {
+      return file.Key.indexOf(filePrefix + projectName + fileSuffix) === 0;
+    })
+    // .tap(console.log)
+    // 2. Download raw files from S3
+    .map(function (file) {
+      // var destination = fs.createWriteStream('./debug/' + file.Key);
+      return {
+        name: file.Key,
+        stream: s3.getObject({Bucket: bucketName, Key: file.Key}).createReadStream()
+      };
+    })
+    // // Debug the incoming stream to the console.
+    // .tap(function (files) {
+    //   files.forEach(function (file) {
+    //     file.stream.on('data', function (data) {
+    //       console.log(data);
+    //     });
+    //   });
+    // })
+    // 3. Pass raw files to zip library
+    .then(function (files) {
+      // var output = fs.createWriteStream(__dirname + '/example-output.zip');
+      var archive = archiver('zip');
+      files.forEach(function (file) {
+        archive.append(file.stream, { name: file.name });
+      });
+      archive.finalize();
+      return archive;
+    });
+}
 
-  // 3. Pass raw files to zip library
-  // 4. Serve zip folder to user
+module.exports = {
+  getZip: getProjectFilesAsZip
+};
+
+// 4. Serve zip folder to user
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var archiver = require('../archiver.js');
+
+setInterval(function () {
+  console.log(require('util').inspect(process.memoryUsage()));
+}, 1000);
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/download/:size', function (req, res) {
+  archiver.getZip(req.params.size).then(function(response) {
+    response.pipe(res);
+  });
 });
 
 module.exports = router;
